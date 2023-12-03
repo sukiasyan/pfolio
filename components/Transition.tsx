29782d32593476e5004071d4cb609ed359cea22d
hsukiasyan <hakob.sukiasyan@pricespider.com>
2023-12-03 10:33:42 +0100

    full site initial

---
+++ components/Transition.tsx
@@ -0,0 +1,51 @@
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+
+const TransitionVariants = {
+  initial: {
+    y: "100%",
+    height: "100%",
+  },
+  animate: {
+    y: "0%",
+    height: "0%",
+  },
+  exit: {
+    y: ["0%", "100%"],
+    height: ["0%", "100%"],
+  },
+};
+
+const Transition = () => {
+  return (
+    <div>
+      <motion.div
+        className="fixed right-0 h-screen w-screen bottom-full z-[30] bg-[#fe6d4e]"
+        variants={TransitionVariants}
+        initial="initial"
+        exit="exit"
+        animate="animate"
+        transition={{ delay: 0.2, duration: 0.6, ease: "easeInOut" }}
+      />
+      <motion.div
+        className="fixed right-0 h-screen w-screen bottom-full z-[20] bg-[#fcfcfc]"
+        variants={TransitionVariants}
+        initial="initial"
+        exit="exit"
+        animate="animate"
+        transition={{ delay: 0.4, duration: 0.6, ease: "easeInOut" }}
+      />
+      <motion.div
+        className="fixed right-0 h-screen w-screen bottom-full z-[5] bg-[#fe6d4e]"
+        variants={TransitionVariants}
+        initial="initial"
+        exit="exit"
+        animate="animate"
+        transition={{ delay: 0.6, duration: 0.6, ease: "easeInOut" }}
+      />
+    </div>
+  );
+};
+
+export default Transition;
